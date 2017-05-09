754800694e4327a78f027aed7fb7b6c9a02486aa
yj.lee <ygee0613@gmail.com>
2017-05-09 11:44:11 +0900

    static example

---
+++ ConsoleApplication7_static/ConsoleApplication7_static/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7_static
+{
+
+
+    class MyClass
+    {
+        public static int number = 12;
+        public static void Print()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+
+    class Program
+    {
+
+        static string Message = "Hello";
+        static void Print() { Console.WriteLine(Message); }
+
+        static void Main(string[] args)
+        {
+
+            Print();
+
+
+            MyClass.Print();
+            Console.WriteLine(MyClass.number);
+
+
+            MyClass.number = 200;
+
+            MyClass test = new MyClass();
+            Console.WriteLine(MyClass.number);
+
+        }
+
+
+    }
+}
