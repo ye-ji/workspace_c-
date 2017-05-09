714e54bef64638bf5c7c8247d2bcbda1c3d8b2ab
yj.lee <ygee0613@gmail.com>
2017-05-09 13:10:04 +0900

    Class

---
+++ ConsoleApplication8Class/ConsoleApplication8Class/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8Class
+{
+    class MyClass
+    {
+        public int number;
+        public MyClass()
+        {
+            number = 12;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass MyTest = new MyClass();
+            MyClass RefClass = MyTest;
+            RefClass.number = 7;
+            Console.WriteLine(MyTest.number);
+        }
+    }
+}
