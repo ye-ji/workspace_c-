0406c1c897b1d1773f286962f12afe03d6104f18
yj.lee <ygee0613@gmail.com>
2017-05-09 13:35:47 +0900

    add get/set

---
+++ ConsoleApplication9/ConsoleApplication9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class MyClass
+    {
+        int count;
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        public MyClass(int count)
+        {
+            this.count = count;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(count);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass Test = new ConsoleApplication9.MyClass(3); // get 
+            Console.WriteLine(Test.Count);
+
+            int Sum = Test.Count + 10;
+            Console.WriteLine(Sum);
+
+            Test.Count = 20; // set
+            Test.Print();
+
+        }
+    }
+}
