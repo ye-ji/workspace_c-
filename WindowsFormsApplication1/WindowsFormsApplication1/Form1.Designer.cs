f2587174648c282597ca80d91046a3372af0be63
yj.lee <ygee0613@gmail.com>
2017-05-09 21:13:55 +0900

    window form

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(261, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 421);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Hello World! C#";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    // partial: class나 구조체, 인터페이스, 메소드의 정의를 둘 이상의 소스 파일로 분할한 경우!!
+    // 규모가 큰 프로젝트일 때 하나의 파일에 다수가 접근할 때 사용됨!
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 윈도우 이벤트 핸들러는 모두 같은 형식!!!!
+        // sender: 이벤트를 호출하는 객체 참조. (이벤트가 어느객체에서 발생했는지에 대해 참조할 수 있게 해줌)
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawString("hello world", Font, Brushes.Black, 10, 10);
+
+            e.Graphics.DrawLine(Pens.Red, 20, 20, 200, 20);
+            e.Graphics.DrawEllipse(Pens.Black, 20, 30, 100, 100);
+            e.Graphics.DrawRectangle(Pens.Black, 150, 20, 100, 100);
+        }
+    }
+}
