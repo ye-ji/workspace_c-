42811fd1f4a3ef1c61afd00d3b6796c88a181d74
yj.lee <ygee0613@gmail.com>
2017-05-09 15:02:57 +0900

    multicast delegate

--- ConsoleApplication7_delegate/ConsoleApplication7_delegate/Program.cs
+++ ConsoleApplication7_delegate/ConsoleApplication7_delegate/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication7_delegate
      * [접근한정자] delegate return형 델리케이트형명(메소드 매개변수);
      * delegate int DelegateType(string Name);
      */
-    delegate void DelegateType(string str);
+    // delegate void DelegateType(string str);
 
     class A
     {
@@ -20,18 +20,52 @@ namespace ConsoleApplication7_delegate
         }
     }
 
+
+    /*
+     * 멀티캐스트 델리케이트
+     * multicast delegate 
+     */
+
+
+    delegate void DelegateType();
+    class C
+    {
+        public void printA()
+        {
+            Console.WriteLine("printA");
+        }
+
+        public void printB()
+        {
+            Console.WriteLine("printB");
+        }
+    }
+
+
+
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            /*
             A Test = new ConsoleApplication7_delegate.A();
             DelegateType delMethod1 = new DelegateType(Test.Print); // C# 1.0 이상
             delMethod1("Hello World");
 
             DelegateType delMethod2 = Test.Print;
             delMethod2("Hello World!!!!!"); // C# 2.0 이상 
+            */
+            // --------------------------------------
 
-
+            C test = new C();
+            DelegateType delFunc = test.printA;
+            delFunc += test.printB;
+            delFunc();
+            delFunc -= test.printB;
+            delFunc();
 
         }
     }
